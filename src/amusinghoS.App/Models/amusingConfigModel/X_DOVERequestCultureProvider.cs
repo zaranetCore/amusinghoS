1b57d67e096417dbd08f79feca534beb7528d804
zaranetcore <zaranet@163.com>
2020-02-19 18:04:33 +0800

    Updated .NET Core Global Localization

---
+++ src/amusinghoS.App/Models/amusingConfigModel/X_DOVERequestCultureProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace amusinghoS.App.Models.amusingConfigModel
+{
+    public class X_DOVERequestCultureProvider : RequestCultureProvider
+    {
+        public override Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+        {
+            var CULTURE_String = "CULTURE";
+            var CultureCookie = httpContext.Request.Cookies[CULTURE_String]?.ToString() ?? "";
+            if (string.IsNullOrWhiteSpace(CultureCookie))
+            {
+                CultureCookie = "zh-CN";
+                httpContext.Response.Cookies.Append(key: CULTURE_String, value: CultureCookie, options: new CookieOptions() { Expires = DateTime.Now.AddYears(1) });
+            }
+            return Task.FromResult(new ProviderCultureResult(CultureCookie));
+        }
+    }
+}
--- src/amusinghoS.App/Startup.cs
+++ src/amusinghoS.App/Startup.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using amusinghoS.App.Models.amusingConfigModel;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace amusinghoS
 {
@@ -22,6 +27,20 @@ namespace amusinghoS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+            services.Configure<RequestLocalizationOptions>(opts =>
+                {
+                    var supportedCultures = new List<CultureInfo>{
+                        new CultureInfo("en-US"),
+                        new CultureInfo("zh-CN")};
+                        opts.SupportedCultures = supportedCultures;
+                        opts.SupportedUICultures = supportedCultures; 
+                        opts.RequestCultureProviders = new List<IRequestCultureProvider>{
+                           new X_DOVERequestCultureProvider()
+                        };
+                });
             services.AddControllersWithViews();
         }
 
@@ -41,7 +60,9 @@ namespace amusinghoS
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseRequestLocalization(
+                options:app.ApplicationServices
+                .GetService<IOptions<RequestLocalizationOptions>>().Value);
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
