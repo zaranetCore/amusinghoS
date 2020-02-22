fa1a15ab1defbcd6c457c15e05334ceaab545194
zaranetcore <zaranet@163.com>
2020-02-22 19:23:56 +0800

    Add Redis

---
+++ src/amusinghoS.Redis/IRedisClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amusinghoS.Redis
+{
+    public interface IRedisClient
+    {
+        string Get(string key);
+        void Set(string key, object t, int expiresSec = 0);
+        T Get<T>(string key) where T : new();
+        Task<string> GetAsync(string key);
+        Task SetAsync(string key, object t, int expiresSec = 0);
+        Task<T> GetAsync<T>(string key) where T : new();
+    }
+}
