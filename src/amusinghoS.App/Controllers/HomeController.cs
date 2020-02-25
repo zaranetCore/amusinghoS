62e45a0adbdb680bba095806e7271c60806555e4
zaranetcore <zaranet@163.com>
2020-02-25 16:54:58 +0800

    Homepage adds pagination support

--- src/amusinghoS.App/Controllers/HomeController.cs
+++ src/amusinghoS.App/Controllers/HomeController.cs
@@ -20,18 +20,32 @@ namespace amusinghoS.Controllers
         public IActionResult Index()
         {
             //默认 8个文章
-            var list = _unitWork.amusingArticleRepository.GetAll().Take(3).ToList();
-            ViewData["ArticleCount"] = list.Count;
-            return View(list);
+            var list = _unitWork.amusingArticleRepository.GetAll();
+            ViewData["Current"] = 1;
+
+            ViewData["NextIsNull"] = "NO";
+
+            return View(list.Take(3).ToList());
         }
         [Route("/Index/{id}")]
         public async Task<IActionResult> Index(int id)
         {
             var list = await _unitWork.amusingArticleRepository
                 .GetListAsync() ;
-            ViewData["ArticleCount"] = list.Count;
-            return View(list.Skip((id - 1) * 3)
-                .Take(3).ToList());
+
+            ViewData["NextIsNull"] = "NO";
+
+            if ((list.Count / 3) + 1 == id)
+            {
+                ViewData["NextIsNull"] = "OK";
+            }
+            ViewData["Current"] = id;
+            list = list.Skip((id - 1) * 3)
+                .Take(3).ToList();
+            if (list.Count == 0)
+                ViewData["NextIsNull"] = "OK";
+
+            return View(list);
         }
     }
 }
